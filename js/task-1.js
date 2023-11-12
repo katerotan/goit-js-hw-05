4371f04a65612e9631f7ad92896214e50d5ce0c7
katerotan <katerinarotan@gmail.com>
2023-11-12 17:12:50 +0200

    Task 1 Done

--- js/task-1.js
+++ js/task-1.js
@@ -1,45 +1,53 @@
-// console.log("Задача 1. Імена користувачів");
+console.log("Задача 1. Імена користувачів");
 
-// Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів. Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
+// 1) Напиши стрілочну функцію getUserNames(users),
+// яка прийматиме один параметр users — масив об’єктів користувачів.
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
+// 2) Функція має повертати масив імен усіх користувачів(властивість name) із масиву users.
 
-// console.log(
-//   getUserNames([
-//   {
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     balance: 2811
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     balance: 3821
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     balance: 3793
-//   },
-//   {
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     balance: 2278
-//   },
-//   {
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     balance: 3951
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     balance: 1498
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     balance: 2764
-//   },
-// ])
-// ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+const getUserNames = (users) => {
+  
+    const names = users.map(user => user.name);
+  
+    return names;
+};
+
+const users = [
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        balance: 2811
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        balance: 3821
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        balance: 3793
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        balance: 2278
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        balance: 3951
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        balance: 1498
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        balance: 2764
+    },
+]; 
+
+console.log(getUserNames(users)); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
